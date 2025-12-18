56e6cc1d40ad4cb0ed653bc31b0bbc1dd7c49c83
Mulloy Morrow <1915420+mulloymorrow@users.noreply.github.com>
2025-12-18 16:39:52 -0500

    Configure for code.mulloymorrow.com custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mulloymorrow.github.io',
+  site: 'https://code.mulloymorrow.com',
   outDir: './docs',
   integrations: [svelte()],
   build: {
