a1ff29e7a594623a078ba58bc8dc174d51cd8d5e
Mulloy Morrow <1915420+mulloymorrow@users.noreply.github.com>
2026-01-22 12:06:14 -0500

    Add sitemap integration to Astro configuration and update dependencies
    - Include '@astrojs/sitemap' integration in the Astro configuration for improved SEO.
    - Update package.json and package-lock.json to add '@astrojs/sitemap' dependency.
    - Remove unused CSS files and clean up assets for better project organization.
    - Update blog and philosophy pages to reflect recent changes and ensure consistency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://code.mulloymorrow.com',
   outDir: './docs',
-  integrations: [svelte()],
+  integrations: [svelte(), sitemap()],
   build: {
     assets: '_assets'
   }
